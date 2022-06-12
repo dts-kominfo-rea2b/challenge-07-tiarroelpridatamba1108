81b6acede6fce4bf33e44a6454795461e8f0efde
tiarroelpridatamba1108 <tiarrotamba24@gmail.com>
2022-06-12 18:48:18 +0700

    Promise Challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (result) => {
+  const resultIXX = await promiseTheaterIXX();
+  const resultVGC = await promiseTheaterVGC();
+  let marah = 0;
+  let tidakMarah = 0;
+  for (let i = 0; i < resultIXX.length; i++) {
+    if (resultIXX[i].hasil === "marah") {
+      marah++;
+    } else {
+      tidakMarah++;
+    }
+  }
+
+  for (let i = 0; i < resultVGC.length; i++) {
+    if (resultVGC[i].hasil === "marah") {
+      marah++;
+    } else {
+      tidakMarah++;
+    }
+  }
+
+  if (result === "marah") return marah;
+  return tidakMarah;
+};
 
 module.exports = {
   promiseOutput,
